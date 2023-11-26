9a0cbbf7b23c77998e7e1dd92be89850bb18b197
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2023-11-25 21:24:08 -0300

    cod_quase_funcional

--- Principal.c
+++ Principal.c
@@ -1,30 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 
-int main(){
-    //uma palavra pré-definida
-    const char palavraSecreta[] = "melancia\0";
-    //acertou começa como falso, sem valor
-    short acertou = NULL;
-    //enforcou começa como positivo
-    short enforcou = 1;
+int main() {
 
-    //primeira coisa a se fazer no while
-    do{
-        //uma variavel do tipo string que guarda somente um caracter
-        char chute;
-        //Lẽ e guarda no ponteiro chute
-        scanf("%c", &chute);
+    char palavrasecreta[20];
+    sprintf(palavrasecreta, "melancia");
 
-        //repete até que a string até chegar ao fim dela
-        for (size_t i = 0; i < strlen(palavraSecreta); i++){
-            //se uma das posições da palavraSecreta tiver a letra escolhida
-            if(palavraSecreta[i] == chute){
-                //escreve a posição encontrada a letra escolhida
-                printf("A posição %zu tem essa letra\n", i);
+    int acertou = 0;
+    int enforcou = 0;
+
+    char chutes[26];
+    int tentativas = 0;
+
+    do {
+
+        for(int i = 0; i < strlen(palavrasecreta); i++) {
+            int achou = 0;
+
+            for(int j = 0; j < tentativas; j++) {
+                if(chutes[j] == palavrasecreta[i]) {
+                    achou = 1;
+                    break;
+                }
+            }
+
+            if(achou) {
+                printf("%c ", palavrasecreta[i]);
+            } else {
+                printf("_ ");
             }
         }
-    //Depois verificar
-    //se (não acertou e não enforcou) faça:
-    }while(!acertou && !enforcou);
+        printf("\n");
+
+        char chute;
+        printf("Qual letra? ");
+        scanf(" %c", &chute);
+
+        chutes[tentativas] = chute;
+        tentativas++;
+
+
+    } while (!acertou && !enforcou);
+
 }
