fd637c7ca43a4bc7fbf93cf00e4c72e7554bd82b
SamuelHenriqueDeMoraisVitrio <samuelhenriquemoraisvitrio@gmail.com>
2023-11-26 12:54:08 -0300

    Tudo_errado

--- Principal.c
+++ Principal.c
@@ -144,3 +144,69 @@ int main() {
 
 }
 */
+/*
+#include <stdio.h>
+#include <string.h>
+
+void abertura() {
+	printf("/****************\n");
+	printf("/ Jogo de Forca *\n");
+	printf("/****************\n\n");
+}
+
+void chuta(char chutes[], int* tentativas) {
+	char chute;
+	printf("Qual letra? ");
+	scanf(" %c", &chute);
+
+	chutes[*tentativas] = chute;
+	(*tentativas)++;
+}
+
+int jachutou(char letra, char* chutes, int tentativas) {
+	int achou = 0;
+	for(int j = 0; j < tentativas; j++) {
+		if(chutes[j] == letra) {
+			achou = 1;
+			break;
+		}
+	}
+
+	return achou;
+}
+
+int main() {
+
+	char palavrasecreta[20];
+	sprintf(palavrasecreta, "MELANCIA");
+
+	int acertou = 0;
+	int enforcou = 0;
+
+	char chutes[26];
+	int tentativas = 0;
+
+	abertura();
+
+	do {
+		
+		printf("Você já deu %d chutes\n", tentativas);
+
+		for(int i = 0; i < strlen(palavrasecreta); i++) {
+
+			if(jachutou(palavrasecreta[i], chutes, tentativas)) {
+				printf("%c ", palavrasecreta[i]);
+			} else {
+				printf("_ ");
+			}
+
+		}
+		printf("\n");
+
+		chuta(chutes, &tentativas);
+
+
+	} while (!acertou && !enforcou);
+
+}
+*/
